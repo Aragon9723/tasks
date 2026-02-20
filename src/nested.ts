e7aa47bd497d70dae4c285f86afe91e9e1b26699
Aragon9723 <160268275+Aragon9723@users.noreply.github.com>
2026-02-20 12:37:04 -0500

    Finished publishAll

--- src/nested.ts
+++ src/nested.ts
@@ -93,7 +93,10 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * Hint: as usual, do not modify the input questions array
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    const deepCopy = questions.map(
+        (question: Question): Question => ({ ...question, published: true }),
+    );
+    return deepCopy;
 }
 
 /***
