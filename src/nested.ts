aaca79283d631218d84a074a28d7d708e5f291a5
Aragon9723 <160268275+Aragon9723@users.noreply.github.com>
2026-02-20 12:35:46 -0500

    Finished makeAnswers

--- src/nested.ts
+++ src/nested.ts
@@ -74,7 +74,17 @@ export function getNames(questions: Question[]): string[] {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    const deepCopy = questions.map(
+        (question: Question): Question => ({ ...question }),
+    );
+    return deepCopy.map(
+        (question: Question): Answer => ({
+            questionId: question.id,
+            text: "",
+            submitted: false,
+            correct: false,
+        }),
+    );
 }
 
 /***
