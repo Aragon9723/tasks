b3e84a6a537f69cafe069436795499ef8e1817e8
Aragon9723 <160268275+Aragon9723@users.noreply.github.com>
2026-03-19 19:41:17 -0400

    Finished ColoredBox

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,12 +4,20 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): React.JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+interface ColoredBoxProps {
+    curColorIndex: number;
+    setColorIndex: (newIndex: number) => void;
+}
+
+function ChangeColor({
+    curColorIndex,
+    setColorIndex,
+}: ColoredBoxProps): React.JSX.Element {
+    // const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     return (
         <Button
             onClick={() => {
-                setColorIndex((1 + colorIndex) % COLORS.length);
+                setColorIndex((1 + curColorIndex) % COLORS.length);
             }}
         >
             Next Color
@@ -17,14 +25,14 @@ function ChangeColor(): React.JSX.Element {
     );
 }
 
-function ColorPreview(): React.JSX.Element {
+function ColorPreview({ curColorIndex }: ColoredBoxProps): React.JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[curColorIndex],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px",
@@ -34,13 +42,22 @@ function ColorPreview(): React.JSX.Element {
 }
 
 export function ColoredBox(): React.JSX.Element {
+    const [curColorIndex, setColorIndex] =
+        useState<number>(DEFAULT_COLOR_INDEX);
+
     return (
         <div>
             <h3>Colored Box</h3>
             <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor
+                    curColorIndex={curColorIndex}
+                    setColorIndex={setColorIndex}
+                ></ChangeColor>
+                <ColorPreview
+                    curColorIndex={curColorIndex}
+                    setColorIndex={setColorIndex}
+                ></ColorPreview>
             </div>
         </div>
     );
